df7d9e2d3aee6810de509e5a9012f2c72f267658
Sudha Saravanan <sudha_sarv@yahoo.com>
2018-01-31 23:29:27 -0600

    allow user to input pyramid height and respond to button click

--- mario.js
+++ mario.js
@@ -11,15 +11,54 @@ function determineHeightAndThenDrawPyramid() {
     console.log("someone invoked the determineHeightAndThenDrawPyramid function!");
 
     // TODO 3
+    var heightElem = document.getElementById("height");
+    var formElem = document.getElementById("draw-form");
+
+// set a handler function for the form's submission event
+formElem.onsubmit = function(event) {
+
+    // QUIZ
+    // what happens if we don't do this?
+    event.preventDefault();
+
+    // QUIZ
+    // what happens if we don't do this?
+    clearError();
+
+    // figure out the height the user typed
+    heightStr = heightElem.value;
+
+    // if they didn't type anything at all, give a different error message,
+    // something like "Please provide a height"
+    if(heightStr == "")
+    {
+        displayError("You didn't type anything. Please enter a height.");
+        return;
+    }
     // figure out the height the user typed (replace the "5" below)
     heightStr = "5";
 
     // here we convert the string to an int
     height = parseInt(heightStr);
 
+    // if the height is not-a-number, yell at them and exit early
+    // negative numbers and zero will also be rejected here
+    if (isNaN(height) || height < 1) {
+        displayError("That's not a valid height.");
+        return;
+    }
+
+    // if the height is absurdly tall, yell at them and exit early
+    var tooTall = 100;
+    if (height > tooTall) {
+        displayError("Are you cray? I can't build a pyramid that tall.");
+        return;
+    }
+
+
     // TODO 2
     // draw the pyramid with the given height
-
+    drawPyramid(height);
 }
 
 
@@ -65,4 +104,5 @@ function determineHeightAndThenDrawPyramid() {
         // insert the paragraph as a child of the container <div>
         document.getElementById("pyramid").appendChild(rowElem);
     }
+}
 }
